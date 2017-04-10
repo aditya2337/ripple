280e5e0a83c2a4bbe3491bfcbaa89c7a9bd12e7a
aditya2337 <aditya.n@gai.co.in>
2017-04-11 01:05:58 +0530

    adding new font

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 class App extends Component {
   render () {
     return (
-      <div className='flex justify-center items-center h-100 tc'>
-        <input type='text' className='tc' placeholder='Enter your name, user' />
+      <div className='flex justify-center items-center h-100 tc f1'>
+        <input type='text' className='tc bn buton-style  bg-transparent' placeholder='Enter your name, user' />
       </div>
     );
   }
