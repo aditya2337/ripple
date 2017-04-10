0174d0bc2d785f5b50cbea6200a05bf08ddbc005
aditya2337 <aditya.n@gai.co.in>
2017-04-10 23:54:00 +0530

    got the initial changes before I begin the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='flex justify-center items-center h-100 tc'>
+        <input type='text' className='tc' placeholder='Enter your name, user' />
       </div>
     );
   }
