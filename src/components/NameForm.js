5fe9df5bff5e4ea2c46db74c694a3ddd2da65188
aditya2337 <aditya.n@gai.co.in>
2017-04-13 23:39:00 +0530

    added the static questions

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -22,13 +22,59 @@ export default class NameForm extends Component {
           'Alex',
           'Jamie'
         ]
+      },
+      question2: {
+        q: `How many ducks does it take
+        to have two ducks behind one duck,
+        one duck in front of two ducks,
+        and two ducks in front of one duck?`,
+        choices: [
+          '9 Ducks',
+          '2 Ducks',
+          '1 Duck',
+          '3 Ducks'
+        ]
+      },
+      question3: {
+        q: `A rooster lays an egg on a roof.
+        The wind is blowing East.
+        Which way will the egg fall?`,
+        choices: [
+          'There is no egg',
+          'West',
+          'South',
+          'North',
+          'East'
+        ]
+      },
+      question4: {
+        q: `Two fathers took there sons fishing.
+        They all caught exactly one fish.
+        When they came back, they had three fish.
+        None of the fish were stolen,
+        eaten or thrown overboard.
+        How come they only had three?`,
+        choices: [
+          'Because it was rotten',
+          'Because they gave one fish away',
+          'Because someone ate it',
+          'Because the grandfather took his son fishing and he took his son fishing'
+        ]
+      },
+      question5: {
+        q: `Which does not belong?`,
+        choices: [
+          'Apple',
+          'Corn',
+          'Tomato',
+          'Banana'
+        ]
       }
     };
   }
 
   handleChange = () => {
     const { username } = this.refs;
-
     if (username.value.length > 0) {
       this.setState({ showButton: true });
     } else {
@@ -38,12 +84,10 @@ export default class NameForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-
   }
 
   render () {
     const { showButton } = this.state;
-
     const Button = (showButton) ? (
       <FloatingActionButton type='submit'>
         <ArrowForward />
